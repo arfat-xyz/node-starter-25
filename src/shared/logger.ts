4e543c50387e52d0592b849965c0d9dccf55a5dc
arfat-xyz <arfatrahman08@gmail.com>
2025-04-04 14:09:16 +0600

    update prettier

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -1,7 +1,7 @@
-import { createLogger, format, transports } from 'winston';
+import { createLogger, format, transports } from "winston";
 const { combine, timestamp, label, printf, prettyPrint } = format;
-import DailyRotateFile from 'winston-daily-rotate-file';
-import path from 'path';
+import DailyRotateFile from "winston-daily-rotate-file";
+import path from "path";
 
 const myFormat = printf(({ level, message, label, timestamp }) => {
   const date = new Date(timestamp as Date);
@@ -12,55 +12,55 @@ const myFormat = printf(({ level, message, label, timestamp }) => {
 });
 
 const logger = createLogger({
-  level: 'info',
+  level: "info",
   format: combine(
-    label({ label: 'Arfat Rahman' }),
+    label({ label: "Arfatur Rahman" }),
     timestamp(),
     prettyPrint(),
     myFormat,
   ),
-  defaultMeta: { service: 'user-service' },
+  defaultMeta: { service: "user-service" },
   transports: [
     new transports.Console(),
     new DailyRotateFile({
       filename: path.join(
         process.cwd(),
-        'logs',
-        'winston',
-        'success',
-        'success-%DATE%.log',
+        "logs",
+        "winston",
+        "success",
+        "success-%DATE%.log",
       ),
-      datePattern: 'YYYY-MM-DD-HH',
+      datePattern: "YYYY-MM-DD-HH",
       zippedArchive: true,
-      maxSize: '20m',
-      maxFiles: '14d',
+      maxSize: "20m",
+      maxFiles: "14d",
     }),
   ],
 });
 
 const errorLogger = createLogger({
-  level: 'error',
+  level: "error",
   format: combine(
-    label({ label: 'Arfat Rahman' }),
+    label({ label: "Arfatur Rahman" }),
     timestamp(),
     prettyPrint(),
     myFormat,
   ),
-  defaultMeta: { service: 'user-service' },
+  defaultMeta: { service: "user-service" },
   transports: [
     new transports.Console(),
     new DailyRotateFile({
       filename: path.join(
         process.cwd(),
-        'logs',
-        'winston',
-        'error',
-        'error-%DATE%.log',
+        "logs",
+        "winston",
+        "error",
+        "error-%DATE%.log",
       ),
-      datePattern: 'YYYY-MM-DD-HH',
+      datePattern: "YYYY-MM-DD-HH",
       zippedArchive: true,
-      maxSize: '20m',
-      maxFiles: '14d',
+      maxSize: "20m",
+      maxFiles: "14d",
     }),
   ],
 });
